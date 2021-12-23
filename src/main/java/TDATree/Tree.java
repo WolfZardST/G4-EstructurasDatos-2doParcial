6fb182dbebbbd778a44f686f5c725aa2e31d89dc
Danae Townsend <danjtown@espol.edu.ec>
2021-12-23 01:32:17 -0500

    Primera aportaçion

--- src/main/java/TDATree/Tree.java
+++ src/main/java/TDATree/Tree.java
@@ -1,6 +1,26 @@
 
 package TDATree;
 
-public class Tree {
+public class Tree<E> {
+    
+    private Node<E> root;
+    
     // DONA
+
+    public Tree(Node<E> root) {
+        this.root = root;
+    }
+
+    public Tree() {
+    }
+
+    public Node<E> getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node<E> root) {
+        this.root = root;
+    }
+    
+    
 }
